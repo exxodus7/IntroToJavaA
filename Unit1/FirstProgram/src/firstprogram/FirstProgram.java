3a79d6971d5309e793fcb4b703b63765876227a1
Lauren Schroeder <exxodus7@gmail.com>
2019-01-24 12:09:42 +0100

    Added FirstProgram

---
+++ Unit1/FirstProgram/src/firstprogram/FirstProgram.java
@@ -0,0 +1,30 @@
+package firstprogram;
+
+import java.util.Scanner;
+
+/**
+ * A program which adds two numbers together.
+ */
+public class FirstProgram {
+
+    /**
+     * @param args Not expecting anything.
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Hello out there!");
+        System.out.println("I will add two numbers for you.");
+        System.out.println("Enter two numbers on a line separated by a space.");
+
+        int n1, n2;
+
+        Scanner writer = new Scanner(System.in);
+        n1 = writer.nextInt();
+        n2 = writer.nextInt();
+
+        System.out.println("The sum of those two numbers is: ");
+        System.out.println(n1 + n2);
+
+        System.out.println("Div test: " + (double) 9 / 2);
+    }
+}
