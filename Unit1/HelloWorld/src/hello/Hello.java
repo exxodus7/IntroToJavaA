d34fb88e7b5b6cbc8155b2ec37f9d28e3a8082de
Lauren Schroeder <exxodus7@gmail.com>
2019-03-01 13:20:40 +0100

    New unit material

---
+++ Unit1/HelloWorld/src/hello/Hello.java
@@ -0,0 +1,9 @@
+package hello;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        // generate some simple output
+        System.out.println("Hello, World!");
+    }
+}
