319f6bf5d053f8b56535cd01fc4b732f17d0a5b0
Lauren Schroeder <exxodus7@gmail.com>
2019-03-14 21:32:08 +0100

    Changing LengthConverter back to incomplete form

--- Unit3/LengthConverter/src/lengthconverter/LengthConverter.java
+++ Unit3/LengthConverter/src/lengthconverter/LengthConverter.java
@@ -11,24 +11,16 @@ public class LengthConverter {
     public static void main(String[] args) {
         
         double cm;
-        int inches, feet, remainder;
-        final double CM_PER_INCH = 2.54;
-        final int IN_PER_FOOT = 12;
+        int inches;
         Scanner in = new Scanner(System.in);
         
         // convert from inches to centimeters.
         System.out.print("How many inches? ");
         inches = in.nextInt();
-        cm = inches * CM_PER_INCH;
-        System.out.printf("%d in = %f cm\n", inches, cm);
+        cm = inches * 2.54;
+        System.out.println(inches + " in = " + cm + " cm");
         
         // convert from centimeters to inches.
-        System.out.print("How many centimeters? ");
-        cm = in.nextDouble();
-        inches = (int) (cm / CM_PER_INCH);
-        feet = inches / IN_PER_FOOT;
-        remainder = inches % IN_PER_FOOT;
-        System.out.printf("%.2f cm = %d ft, %d in\n", cm, feet, remainder);
     }
 
 }
