79355905b0c2a5fea4933bcc1ce279d6bd7467ee
Lauren Schroeder <exxodus7@gmail.com>
2019-03-20 14:53:11 +0100

    Reset LengthConverter

--- Unit3/LengthConverter/src/lengthconverter/LengthConverter.java
+++ Unit3/LengthConverter/src/lengthconverter/LengthConverter.java
@@ -13,12 +13,11 @@ public class LengthConverter {
         double cm;
         int inches;
         Scanner in = new Scanner(System.in);
-        final double CM_TO_INCH = 2.54;
         
         // convert from inches to centimeters.
         System.out.print("How many inches? ");
         inches = in.nextInt();
-        cm = inches * CM_TO_INCH;
+        cm = inches * 2.54;
         System.out.println(inches + " in = " + cm + " cm");
         
         // convert from centimeters to inches.
