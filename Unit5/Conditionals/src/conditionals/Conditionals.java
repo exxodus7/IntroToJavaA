f211b9e5e70003497907bd8f8c2f221c4babda3a
Lauren Schroeder <exxodus7@gmail.com>
2019-04-06 14:26:17 +0200

    Fixed Conditionals so that it will build on different computers

---
+++ Unit5/Conditionals/src/conditionals/Conditionals.java
@@ -0,0 +1,25 @@
+package conditionals;
+
+import java.util.Scanner;
+
+/**
+ * This class is meant to illustrate basic conditional statements.
+ */
+public class Conditionals {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter two whole numbers: ");
+        int x = in.nextInt();
+        int y = in.nextInt();
+
+        System.out.println("x == y? " + (x == y));  // x is equal to y
+        System.out.println("x != y? " + (x != y));  // x is not equal to y
+        System.out.println("x > y? " + (x > y));    // x is greater than y
+        System.out.println("x < y? " + (x < y));    // x is less than y
+        System.out.println("x >= y? " + (x >= y));  // x is greater than or equal to y
+        System.out.println("x <= y? " + (x <= y));  // x is less than or equal to y
+    }
+
+}
